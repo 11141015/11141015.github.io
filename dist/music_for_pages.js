c62942066a3bc5604d41eea48c4156967e98ff0a
11141015 <1019766912@qq.com>
2020-11-12 09:19:02 +0800

    Site updated: 2020-11-12 09:19:01

--- dist/music_for_pages.js
+++ dist/music_for_pages.js
@@ -5,11 +5,11 @@ const ap = new APlayer({
     lrcType: 3,
     audio: [
 	{
-        name: "想见你想见你想见你",
-        artist: '八三夭',
-        url: './想見你想見你想見你.mp3',
-        cover: '想見你想見你想見你.jpg',
-	lrc: '想見你想見你想見你.lrc'
+        name: "sleepyhead",
+        artist: 'submarine boy (Feat. Ocoenia)',
+        url: './sleepyhead.mp3',
+        cover: 'sleepyhead.jpg',
+	lrc: 'sleepyhead.lrc'
       }
     ]
 });
